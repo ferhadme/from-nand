e967ac664bb304f5b99de411b93c705bc88ab610
Ferhad Mehdizade <frhdmehdiyev@gmail.com>
2024-06-28 12:31:39 +0400

    full adder

---
+++ full_adder.v
@@ -0,0 +1,22 @@
+`include "half_adder.v"
+
+// a + b + cin = sum (carry)
+// sum0 (carry0) = a half_adder b
+// sum (carry1) = sum0 + cin
+// cout = carry0 or carry1
+module full_adder(
+  input wire a,
+  input wire b,
+  input wire cin,
+  output wire sum,
+  output wire cout
+);
+  wire s0;
+  wire c0;
+  wire c1;
+
+  half_adder ha0(a, b, s0, c0);
+  half_adder ha1(s0, cin, sum, c1);
+
+  or_0 or0(c0, c1, cout);
+endmodule
---
+++ full_adder_testbench.v
@@ -0,0 +1,60 @@
+`include "full_adder.v"
+`include "assert.v"
+
+module full_adder_testbench;
+  reg a, b, cin;
+  wire sum, cout;
+
+  full_adder fa(a, b, cin, sum, cout);
+
+  initial begin
+    a=0;
+    b=0;
+    cin=0;
+    #10;
+    `assert(sum, 0);
+    `assert(cout, 0);
+     a=0;
+     b=0;
+     cin=1;
+     #10;
+     `assert(sum, 1);
+     `assert(cout, 0);
+     a=0;
+     b=1;
+     cin=0;
+     #10;
+     `assert(sum, 1);
+     `assert(cout, 0);
+     a=0;
+     b=1;
+     cin=1;
+     #10;
+     `assert(sum, 0);
+     `assert(cout, 1);
+     a=1;
+     b=0;
+     cin=0;
+     #10;
+     `assert(sum, 1);
+     `assert(cout, 0);
+     a=1;
+     b=0;
+     cin=1;
+     #10;
+     `assert(sum, 0);
+     `assert(cout, 1);
+     a=1;
+     b=1;
+     cin=0;
+     #10;
+     `assert(sum, 0);
+     `assert(cout, 1);
+     a=1;
+     b=1;
+     cin=1;
+     #10;
+     `assert(sum, 1);
+     `assert(cout, 1);
+  end
+endmodule
